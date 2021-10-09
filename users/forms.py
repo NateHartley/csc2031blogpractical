2b5ebc8c4e08026c2384c89ac11507eece38e3c4
Annabel Hartley (UG) <c0031586@newcastle.ac.uk>
2021-10-09 12:15:36 +0100

    Password must be the same as the confirmation password.

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email, Length
+from wtforms.validators import Required, Email, Length, EqualTo
 
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[Required(), Email()])
     password = PasswordField(validators=[Required(), Length(min=8, max=15, message='Password must be between 8 and 15 characters in length.')])
+    confirm_password = PasswordField(validators=[Required(), EqualTo('password', message='Both password fields must be equal!')])
     submit = SubmitField()
