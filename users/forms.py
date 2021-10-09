a17cb00563448002a9161d595549d1fe6b84bd91
Annabel Hartley (UG) <c0031586@newcastle.ac.uk>
2021-10-09 12:07:40 +0100

    The email and password blocks must be filled and the email must be a valid email address.

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email
 
 
 class RegisterForm(FlaskForm):
-    username = StringField()
-    password = PasswordField()
+    username = StringField(validators=[Required(), Email()])
+    password = PasswordField(validators=[Required()])
     submit = SubmitField()
