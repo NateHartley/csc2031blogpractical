71f970bfaad7889a312b79b5f56c57952e32d53f
Annabel Hartley (UG) <c0031586@newcastle.ac.uk>
2021-10-09 12:09:05 +0100

    Password must be between 8 and 15 characters.

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, Length
 
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[Required(), Email()])
-    password = PasswordField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=8, max=15, message='Password must be between 8 and 15 characters in length.')])
     submit = SubmitField()
