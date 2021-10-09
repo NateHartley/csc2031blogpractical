d162eefef1636abf65536c224c1be5703021e947
Annabel Hartley (UG) <c0031586@newcastle.ac.uk>
2021-10-09 11:26:17 +0100

    Created Blueprints so that app.py just contains configuration code and the view function index, and handling users and blog posts are separated into their respective blueprints.

--- app.py
+++ app.py
@@ -6,22 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('index.html', name='Nate')
-
-
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
+    return render_template('index.html', name='Alice')
 
 
 if __name__ == '__main__':
@@ -31,5 +16,12 @@ if __name__ == '__main__':
     free_socket.listen(5)
     free_port = free_socket.getsockname()[1]
     free_socket.close()
+
+    # BLUEPRINTS
+    from users.views import users_blueprint
+    from blog.views import blog_blueprint
+
+    app.register_blueprint(users_blueprint)
+    app.register_blueprint(blog_blueprint)
 
     app.run(host=my_host, port=free_port, debug=True)
