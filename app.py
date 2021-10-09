9f45944d742b73f5058ee801cf70664b48c95a55
Annabel Hartley (UG) <c0031586@newcastle.ac.uk>
2021-10-09 11:01:43 +0100

    Added HTML file named index. Displays the title, a header and a subheader. We will see this page when we first run the program. To view or render this HTML file, I imported render_template and changed the index function in app.py Also will display the name after the sub header.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import socket
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():  # put application's code here
-    return 'Welcome to the CSC2031 Blog!'
+    return render_template('index.html', name='Nate')
 
 
 if __name__ == '__main__':
