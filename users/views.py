2a3943f62dd8580a6a2c3265ee661d4b3067fc0d
NateHartley <MiniMax-03>
2021-10-21 12:28:35 +0100

    Logs users signing in and out

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
+import logging
 from datetime import datetime
-from flask import Blueprint, render_template, flash, redirect, url_for, session
-from flask_login import login_user, logout_user, login_required
+from flask import Blueprint, render_template, flash, redirect, url_for, session, request
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
 from app import db
 from blog.views import blog
@@ -16,16 +17,24 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
+        # if this returns a user, then the email already exists in database
         user = User.query.filter_by(username=form.username.data).first()
 
+        # if a user is found redirect user back to signup page so user can try again
         if user:
             flash('Username address already exists')
             return render_template('register.html', form=form)
 
-        new_user = User(username=form.username.data, password=form.password.data, pinkey=form.pinkey.data)
+        # create a new user with the form data
+        new_user = User(username=form.username.data,
+                        password=form.password.data,
+                        pinkey=form.pinkey.data)
 
+        # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
+
+        logging.warning('SECURITY - User registration [%s, %s]', form.username.data, request.remote_addr)
 
         return redirect(url_for('users.login'))
 
@@ -34,7 +43,6 @@ def register():
 
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-
     # if session attribute logins does not exist create attribute logins
     if not session.get('logins'):
         session['logins'] = 0
@@ -63,27 +71,25 @@ def login():
 
             return render_template('login.html', form=form)
 
-        if pyotp.TOTP(user.pinkey).verify(form.pinkey.data):
-
-            # if user is verified reset login attempts to 0
+        if pyotp.TOTP(user.pinkey).verify(form.pin.data):
             session['logins'] = 0
-
             login_user(user)
-
             user.last_logged_in = user.current_logged_in
             user.current_logged_in = datetime.now()
-            db.session.add(user)
             db.session.commit()
+            logging.warning('SECURITY - Log in [%s, %s, %s]', current_user.id, current_user.username, request.remote_addr)
+            return blog()
 
         else:
             flash("You have supplied an invalid 2FA token!", "danger")
 
-        return blog()
     return render_template('login.html', form=form)
 
 
 @users_blueprint.route('/logout')
 @login_required
 def logout():
+    logging.warning('SECURITY - Log out [%s, %s, %s]', current_user.id, current_user.username, request.remote_addr)
+
     logout_user()
     return redirect(url_for('index'))
